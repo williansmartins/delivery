dc2e385061d451aff7dc105173bb8d41a66c5805
Martins de Morais <wsms@gft.com>
2019-11-13 17:05:51 -0300

    removendo dao

--- src/main/java/com/capgemini/delivery/controller/ProdutoController.java
+++ src/main/java/com/capgemini/delivery/controller/ProdutoController.java
@@ -1,7 +1,5 @@
 package com.capgemini.delivery.controller;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,117 +11,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.capgemini.delivery.dao.ProdutoDAO;
-import com.capgemini.delivery.model.Batata;
-import com.capgemini.delivery.model.Bebida;
-import com.capgemini.delivery.model.Cliente;
-import com.capgemini.delivery.model.Imagem;
 import com.capgemini.delivery.model.Produto;
-import com.capgemini.delivery.model.Stock;
-import com.capgemini.delivery.model.StockDetail;
-import com.capgemini.delivery.model.Tag;
-import com.capgemini.delivery.model.Telefone;
 import com.capgemini.delivery.model.Tipo;
-import com.capgemini.delivery.repository.ClienteRepository;
-import com.capgemini.delivery.repository.ImagensRepository;
 import com.capgemini.delivery.repository.ProdutoRepository;
-import com.capgemini.delivery.repository.StockRepository;
-
-
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutoController {
 
-	private static ProdutoDAO dao = new ProdutoDAO();
-	
-	@Autowired
-	private ImagensRepository imagemRepository;
-	
-	@Autowired
-	private StockRepository stockRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-	
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
-
-
-	@RequestMapping(value = "salvar", method = RequestMethod.GET)
-    @ResponseBody
-    public String teste() {
-		Produto p = new Produto();
-		p.setTitulo("Protudo novo");
-		
-		produtoRepository.save(p);
-		return "sucesso!";
-	}
-	
 
 	@RequestMapping(value = "", method = RequestMethod.GET)
     @ResponseBody
-    public List<Produto> buscarTodosProdutos() {
-		List<Produto> produtos = dao.buscarTodos();
-		
-		Imagem imagem = new Imagem();
-		imagem.setUrl("https://www.placecage.com/100/100");
-		
-		
-		List<Tag> tags = new ArrayList<Tag>();
-		Tag tag1 = new Tag();
-		tag1.setTag("MAE");
-		tags.add(tag1);
-		
-		Tag tag2 = new Tag();
-		tag2.setTag("FILHO");
-		tags.add(tag2);
-		imagem.setTags(tags);
-		
-		imagemRepository.save(imagem);
-		
-		///////////////////////////////////////////
-		System.out.println("Hibernate one to one (Annotation)");
-
-		Stock stock = new Stock();
-
-		stock.setStockCode("7052");
-		stock.setStockName("PADINI");
-
-		StockDetail stockDetail = new StockDetail();
-		stockDetail.setCompName("PADINI Holding Malaysia");
-		stockDetail.setCompDesc("one stop shopping");
-		stockDetail.setRemark("vinci vinci");
-		stockDetail.setListedDate(new Date());
-
-		stock.setStockDetail(stockDetail);
-		stockDetail.setStock(stock);
-
-		stockRepository.save(stock);
-		
-		/////////////////////////////////////////
-		System.out.println("Hibernate one to one (Annotation)");
-
-		Cliente cliente = new Cliente();
-
-		cliente.setCpf("2131231");
-
-		Telefone telefone = new Telefone();
-		telefone.setTelefoneCelular("123");
-		telefone.setTelefoneResidencial("456");
-
-		cliente.setTelefone(telefone);
-		telefone.setCliente(cliente);
-
-		clienteRepository.save(cliente);
-		
-		return produtos;
+    public Iterable<Produto> buscarTodosProdutos() {
+		return produtoRepository.findAll();
     }
 	
 	@RequestMapping(value = "batatas", method = RequestMethod.GET)
     @ResponseBody
-    public List<Produto> buscarTodasBatatas() {
+    public List<Produto> buscarTodasBatatas(String tipo) {
 		List<Produto> list = produtoRepository.findByTipo(Tipo.BATATA);
 		return list;
     }
@@ -138,33 +45,47 @@ public class ProdutoController {
 	@RequestMapping(value = "adicionais", method = RequestMethod.GET)
     @ResponseBody
     public List<Produto> buscarTodosAdicionais() {
-        List<Produto> adicionais = dao.buscarTodosAdicionais();
-        return adicionais;
+		List<Produto> list = produtoRepository.findByTipo(Tipo.ADICIONAL);
+		return list;
     }
 
     @RequestMapping(value = "{id}", method = RequestMethod.GET)
     @ResponseBody
-    Optional<Produto> buscarUmProduto(@PathVariable Long id) {
+    public Optional<Produto> buscarUmProduto(@PathVariable Long id) {
     	Optional<Produto> produto = produtoRepository.findById(id);
     	return produto;
     }
     
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    void  excluirProduto(@PathVariable Long id) {
+    public void excluirProduto(@PathVariable Long id) {
     	produtoRepository.deleteById(id);
     	
     }
     
     @RequestMapping(value = "" , method = RequestMethod.POST)
     @ResponseBody
-    Produto inserirProduto(@RequestBody Produto produto) {
+    public Produto inserirProduto(@RequestBody Produto produto) {
     	return produtoRepository.save(produto);
     }
     
     @RequestMapping(value = "{id}" , method = RequestMethod.PUT)
     @ResponseBody
-    Produto atualizarProduto(@PathVariable Long id,@RequestBody Produto produto) {
-    	return produtoRepository.save(produto);
+    public Produto atualizarProduto(@PathVariable Long id,@RequestBody Produto produtoDoFront) {
+    	tratarCampos(id, produtoDoFront);
+    	return produtoRepository.save(produtoDoFront);
     }
+
+	private void tratarCampos(Long id, Produto produtoDoFront) {
+		Optional<Produto> produtoDoBack = produtoRepository.findById(id);
+    	Produto produtoDoBackConvertido = produtoDoBack.get();
+    	
+    	if(produtoDoFront.getTitulo().isEmpty()) {produtoDoBackConvertido.setTitulo(produtoDoFront.getTitulo());};
+    	if(produtoDoFront.getPreco() == 0) {produtoDoBackConvertido.setPreco(produtoDoFront.getPreco());};
+    	if(produtoDoFront.getObervacao().isEmpty()) {produtoDoBackConvertido.setObervacao(produtoDoFront.getObervacao());};
+    	if(produtoDoFront.getTipo() == null) {produtoDoBackConvertido.setTipo(produtoDoFront.getTipo());};
+    	
+    	if(produtoDoFront.getIngredientes() == null) {produtoDoBackConvertido.setIngredientes(produtoDoFront.getIngredientes());};
+    	if(produtoDoFront.getAdicionais() == null) {produtoDoBackConvertido.setAdicionais(produtoDoFront.getAdicionais());};
+	}
 }
--- src/main/java/com/capgemini/delivery/repository/StockRepository.java
+++
@@ -1,8 +0,0 @@
-package com.capgemini.delivery.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.capgemini.delivery.model.Stock;
-
-public interface StockRepository extends CrudRepository<Stock, Long> {
-}
