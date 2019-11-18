83d69c652f402b9d4f37cb55fa13e9c14fc90705
Martins de Morais <wsms@gft.com>
2019-11-18 11:20:51 -0300

    add angular

--- frontend/js/app/controller.js
+++ frontend/js/app/controller.js
@@ -2,15 +2,15 @@ angular.module('principal')
 .controller('Controller', ['$scope',  '$http', 
 	function ($scope, $http) {
 
-    $scope.$itens = null;
-    $scope.tela = 0;
+    $scope.itens = null;
+    $scope.tela = 'lista';
 
     $scope.mudarTela = function(tela){
       $scope.tela = tela;
     }
 
     init = function() {
-        $http.get("https://jsonplaceholder.typicode.com/todos/1")
+        $http.get("http://localhost:8080/produtos")
           .then(function(response) {
             $scope.itens = response.data;
         });
