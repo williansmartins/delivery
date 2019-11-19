3405a30b65767f256fd81d51d6f2077245936a4e
Willians Martins <professorwillians@gmail.com>
2019-11-18 21:44:53 -0300

    interagindo com contador e calculando preco individual

--- frontend/js/app/controller.js
+++ frontend/js/app/controller.js
@@ -2,26 +2,32 @@ angular.module('principal')
 .controller('Controller', ['$scope',  '$http', 
 	function ($scope, $http) {
 
-    $scope.itens = null;
-    $scope.tela = 'lista';
-    $scope.quantidade = 0;
+    $scope.produtos = null;
+    $scope.tela = 'pedido';
 
     $scope.mudarTela = function(tela){
       $scope.tela = tela;
     }
 
-    $scope.somarUm = function(){
-      $scope.quantidade++;
+    $scope.somarUm = function(batata){
+      batata.quantidade++;
     }
 
-    $scope.subtrairUm = function(){
-      $scope.quantidade--;
+    $scope.subtrairUm = function(batata){
+      batata.quantidade--;
+    }
+
+    var inicializarProdutos = function(){
+        angular.forEach($scope.produtos, function(value, key){
+            value.quantidade = 0;
+        });
     }
 
     init = function() {
         $http.get("http://localhost:8080/produtos")
           .then(function(response) {
-            $scope.itens = response.data;
+            $scope.produtos = response.data;
+            inicializarProdutos();
         });
 
 
