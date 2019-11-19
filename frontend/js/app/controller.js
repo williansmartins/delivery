29b878ebc1eff72f8fa3579ad4186eef94dc99b6
Willians Martins <professorwillians@gmail.com>
2019-11-18 22:26:15 -0300

    dinamizando o active do menu

--- frontend/js/app/controller.js
+++ frontend/js/app/controller.js
@@ -4,22 +4,37 @@ angular.module('principal')
 
     $scope.produtos = null;
     $scope.tela = 'pedido';
+    $scope.telaPedido = 'batatas';
 
     $scope.mudarTela = function(tela){
       $scope.tela = tela;
+    }
+
+    $scope.mudarTelaPedido = function(telaPedido){
+      $scope.telaPedido = telaPedido;
     }
 
     $scope.somarUm = function(batata){
       batata.quantidade++;
     }
 
+
     $scope.subtrairUm = function(batata){
       batata.quantidade--;
+    }
+
+    $scope.somarUmAdicional = function(batata, adicional){
+      batata.adicionalNumber = adicional;
     }
 
     var inicializarProdutos = function(){
         angular.forEach($scope.produtos, function(value, key){
             value.quantidade = 0;
+            value.adicional1 = 0;
+            value.adicional2 = 0;
+            value.adicional3 = 0;
+            value.adicional4 = 0;
+            value.adicional5 = 0;
         });
     }
 
