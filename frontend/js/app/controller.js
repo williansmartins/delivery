c8a3e95ff75acf456207f34d1658ad8e91fd7c69
Willians Martins <professorwillians@gmail.com>
2019-11-18 21:21:27 -0300

    interagindo com quantidade

--- frontend/js/app/controller.js
+++ frontend/js/app/controller.js
@@ -4,9 +4,18 @@ angular.module('principal')
 
     $scope.itens = null;
     $scope.tela = 'lista';
+    $scope.quantidade = 0;
 
     $scope.mudarTela = function(tela){
       $scope.tela = tela;
+    }
+
+    $scope.somarUm = function(){
+      $scope.quantidade++;
+    }
+
+    $scope.subtrairUm = function(){
+      $scope.quantidade--;
     }
 
     init = function() {
